a4d399df002db40259a1a82efecdc711cfd49dc6
Sreedevi <sreedevivp4u@gmail.com>
2018-10-02 18:52:11 +0100

    images upload

---
+++ WpfTreeView/HeaderToImageConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfTreeView
+{
+    [ValueConversion(typeof(string),typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        /// <summary>
+        /// Converts full path to specific image type drive,folder or file
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Get the full path
+            var path = (string)value;
+
+            //If the path is null,ignore
+            if (path == null)
+                return null;
+            //Get the name of the file/folder
+
+            //By default we persume an image
+            var image = "Images/file.png";
+
+            var name = MainWindow.GetFileFolderName(path);
+            //if the name is blank,we persume it's a drive as we cannot have a blank file or folder name
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder-closed.png";
+
+        
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
